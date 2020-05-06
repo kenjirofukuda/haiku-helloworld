59a99bddeb2951faa32226fc17ecde10594a72e5
kenjirofukuda <kenjirofukuda@gmail.com>
2020-05-06 20:49:03 +0900

    direct draw

--- src/SimpleWindow.cpp
+++ src/SimpleWindow.cpp
@@ -3,8 +3,10 @@
 
 class SimpleWindow : public BWindow {
 public:
-                    SimpleWindow(BRect frame);
-    // virtual bool    QuitRequested();    
+    SimpleWindow(BRect frame);
+    // virtual bool    QuitRequested();
+    void Quit();
+    void Show();    
 };
 
 
@@ -12,7 +14,32 @@ SimpleWindow::SimpleWindow(BRect frame)
     : BWindow(frame, "Simple Window",
               B_TITLED_WINDOW,
               B_NOT_RESIZABLE | B_NOT_ZOOMABLE) 
-{    
+{
+    BView* view;
+
+    view = new BView(this->Bounds(), "content", B_FOLLOW_ALL_SIDES, 0);
+    this->AddChild(view);    
+}
+
+
+void
+SimpleWindow::Show()
+{
+    BView* view = this->FindView("content");
+    rgb_color currColor = view->HighColor();
+
+    BWindow::Show();
+    ::snooze(40 * 1000);
+    
+    this->Lock();
+    view->SetHighColor(0xDD, 0xDD, 0xDD);
+    view->FillRect(view->Frame());
+    view->SetHighColor(currColor);
+    view->DrawString("This is the by first app", BPoint(10, 10));
+    view->SetHighColor(255, 0, 0);
+    view->FillRect(BRect(110, 30, 140, 60));
+    this->Unlock();
+    this->Flush();    
 }
 
 
@@ -22,6 +49,14 @@ SimpleWindow::SimpleWindow(BRect frame)
 //     be_app->PostMessage(B_QUIT_REQUESTED);
 //     return true;
 // }
+
+void
+SimpleWindow::Quit()
+{
+    be_app->PostMessage(B_QUIT_REQUESTED, be_app);
+    BWindow::Quit();    
+}
+
 
 
 class App : public BApplication {
@@ -41,7 +76,7 @@ App::App() : BApplication("application/x-vnd.dw-TestApp")
 void
 App::ReadyToRun()
 {
-    SimpleWindow *window = new SimpleWindow(BRect(0, 0, 640, 480));
+    SimpleWindow *window = new SimpleWindow(BRect(0, 0, 200, 120));
     window->CenterOnScreen();    // Haiku Only
     window->Show();
 }
