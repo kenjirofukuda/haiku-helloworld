40ab188d8c0f18e913a317097b756dc234f6b755
kenjirofukuda <kenjirofukuda@gmail.com>
2020-05-10 15:53:51 +0900

    use ButtonView

---
+++ src/ButtonView.cpp
@@ -0,0 +1,32 @@
+#ifndef _SIMPLE_VIEW_H_
+#include "ButtonView.h"
+#endif
+
+#include <interface/Button.h>
+#include <interface/StringView.h>
+
+ButtonView::ButtonView(BRect frame, const char *name,
+               uint32 resizeMask, uint32 flags)
+    : BView(frame, name, resizeMask, flags)
+{
+    BButton*  button;
+    BMessage* message;
+
+    button = new BButton(BRect(20, 30, 70, 60), "hide button", "Hide", nullptr);
+    message = new BMessage(B_MINIMIZE);
+    message->AddBool("minimize", true);
+    button->SetMessage(message);
+    AddChild(button);
+    
+    button = new BButton(BRect(90, 30, 140, 60), "close button", "Close", nullptr);
+    message = new BMessage(B_QUIT_REQUESTED);
+    button->SetMessage(message);
+    AddChild(button);
+
+    AddChild(new BStringView(BRect(10, 10, 200, 25),
+				   "caption", "This is my 4th app."));
+    
+    SetViewColor(0xDD, 0xDD, 0xDD);
+}
+
+
--- src/SimpleWindow.cpp
+++ src/SimpleWindow.cpp
@@ -2,16 +2,16 @@
 #include <app/Application.h>
 #include "KGCircle.h"
 #include "SimpleWindow.h"
-#include "SimpleView.h"
+#include "ButtonView.h"
 
 SimpleWindow::SimpleWindow(BRect frame)
     : BWindow(frame, "Simple Window",
               B_TITLED_WINDOW,
               B_NOT_RESIZABLE | B_NOT_ZOOMABLE) 
 {
-    SimpleView* view;
+    ButtonView* view;
 
-    view = new SimpleView(Bounds(), "content",
+    view = new ButtonView(Bounds(), "content",
                           B_FOLLOW_ALL_SIDES, B_WILL_DRAW);
     AddChild(view);    
 }
